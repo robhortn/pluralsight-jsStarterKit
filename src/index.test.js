cdf1fb0e5aed8278e837d1d0e2e8c72dd3414e61
robhortn <robhorton@outlook.com>
2016-12-14 21:18:01 -0600

    Adding unit test for index.html

--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,22 @@
 import {expect} from 'chai';
+import jsdom from 'jsdom';
+import fs from 'fs';
 
 describe('Our first test', function(){
 	it('should pass', function() {
 		expect(true).to.equal(true);
 	});
 });
+
+describe('index.html', function(){
+	it('should say hello', function(done) {
+		const index = fs.readFileSync('./src/index.html', "utf-8");
+		jsdom.env(index, function(err, window) {
+			const h1 = window.document.getElementsByTagName('h1')[0];
+			expect(h1.innerHTML).to.equal("Pluralsight: JS Starter Kit");
+			done();
+			window.close();
+		});
+	});
+});
+
