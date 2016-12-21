7be7ff297150bcede9647c17a0bb8697978e9f25
robhortn <robhorton@outlook.com>
2016-12-20 20:54:29 -0600

    Adding mock api

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -19,6 +19,15 @@ app.get('/', function(req, res){
 	res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', function(req, res){
+	// Hard coding for simplicity; pretend this is hitting a DB.
+	res.json([
+		{"id": 1, "firstName": "Rob", "lastName": "Horton", "email": "robhorton@outlook.com"},
+		{"id": 2, "firstName": "Heather", "lastName": "Horton", "email": "heatherhorton@outlook.com"},
+		{"id": 3, "firstName": "Joshua", "lastName": "Horton", "email": "joshuahorton@outlook.com"}
+	]);
+});
+
 app.listen(port, function(err){
 	if (err) {
 		console.log(err);
